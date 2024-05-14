791198727d40720e603715920f8fe9e304e390a6
JaeSunEo <wotjs020708@gmail.com>
2024-05-14 09:58:03 +0900

    DetailViewController initialize 추가

--- JRNL-Codeonly/JournalList Screen/JournalListViewController.swift
+++ JRNL-Codeonly/JournalList Screen/JournalListViewController.swift
@@ -56,7 +56,8 @@ class JournalListViewController: UIViewController, UITableViewDataSource, UITabl
     
     // MARK: - UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let journalDetailViewController = JournalDetailViewController()
+        let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+        let journalDetailViewController = JournalDetailViewController(journalEntry: journalEntry)
         show(journalDetailViewController, sender: self)
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
