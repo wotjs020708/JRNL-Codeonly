fc264995b869b0c62f2a00149bf324c0519d510f
JaeSunEo <wotjs020708@gmail.com>
2024-05-14 10:50:53 +0900

    dateLabel 뿥이기

--- JRNL-Codeonly/JournalDetailViewController.swift
+++ JRNL-Codeonly/JournalDetailViewController.swift
@@ -12,6 +12,7 @@ class JournalDetailViewController: UITableViewController {
   
     private lazy var datelabel: UILabel = {
         let label = UILabel()
+        label.text = "Date"
         label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
         label.textAlignment = .right
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -89,6 +90,17 @@ class JournalDetailViewController: UITableViewController {
     
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+         switch indexPath.row {
+         case 1:
+             cell.contentView.addSubview(datelabel)
+             NSLayoutConstraint.activate([
+                datelabel.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor),
+                datelabel.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 16),
+                datelabel.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -16)
+             ])
+         default:
+             print("others")
+         }
      
 //      Configure the cell...
      
